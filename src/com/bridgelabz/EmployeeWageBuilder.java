776ac02e2d15d6c9ffaf2a936337c34a0229dd2c
shahazad08 <sk.shahazad@gmail.com>
2022-04-23 15:25:23 +0530

    Added: Part time functionality added

--- src/com/bridgelabz/EmployeeWageBuilder.java
+++ src/com/bridgelabz/EmployeeWageBuilder.java
@@ -6,16 +6,21 @@ public class EmployeeWageBuilder {
 
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Computation");
-        int IS_FULL_TIME = 1;
+        int IS_FULL_TIME = 2;
+        int IS_PART_TIME = 1;
         int EMP_RATE_PER_HOUR = 20;
         int empHours = 0;
         int empWage = 0;
         Random random = new Random();
-        int empCheck = random.nextInt(2);
+        int empCheck = random.nextInt(3);
         if (empCheck == IS_FULL_TIME) {
             empHours = 8;
             System.out.println("Employee is Present");
-        } else {
+        } else if(empCheck == IS_PART_TIME) {
+            empHours = 4;
+            System.out.println("Employee is in Part Time");
+        }
+        else {
             empHours = 0;
             System.out.println("Employee is Absent");
         }
