211aec558e90d5baa222d83777632b69dbbf4ecc
shahazad08 <sk.shahazad@gmail.com>
2022-04-23 15:37:03 +0530

    Modified: Switch case is replaced with if else

--- src/com/bridgelabz/EmployeeWageBuilder.java
+++ src/com/bridgelabz/EmployeeWageBuilder.java
@@ -3,26 +3,26 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class EmployeeWageBuilder {
+    public static final int IS_FULL_TIME = 2;
+    public static final int IS_PART_TIME = 1;
+    public static final int EMP_RATE_PER_HOUR = 20;
 
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Computation");
-        int IS_FULL_TIME = 2;
-        int IS_PART_TIME = 1;
-        int EMP_RATE_PER_HOUR = 20;
+
         int empHours = 0;
         int empWage = 0;
         Random random = new Random();
         int empCheck = random.nextInt(3);
-        if (empCheck == IS_FULL_TIME) {
-            empHours = 8;
-            System.out.println("Employee is Present");
-        } else if(empCheck == IS_PART_TIME) {
-            empHours = 4;
-            System.out.println("Employee is in Part Time");
-        }
-        else {
-            empHours = 0;
-            System.out.println("Employee is Absent");
+        switch (empCheck) {
+            case IS_FULL_TIME:
+                empHours = 8;
+                break;
+            case IS_PART_TIME:
+                empHours = 4;
+                break;
+            default:
+                empHours = 0;
         }
         empWage = empHours * EMP_RATE_PER_HOUR;
         System.out.println("The daily wages of an employee is : " + empWage);
